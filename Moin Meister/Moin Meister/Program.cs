3dbf54d3fad5e0dc0baf37f2a58ea605b1da2717
Marvin20212 <marvin.mueller@student.jade-hs.de>
2021-03-11 14:59:34 +0100

    Update Richtig

---
+++ Moin Meister/Moin Meister/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moin_Meister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Moin Meister");
+            Console.ReadKey();
+        }
+    }
+}
